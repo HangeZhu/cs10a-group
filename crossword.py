32dbec89608f0e789f84d1e8f011b9c067346379
HangeZhu <kaixizhu@brandeis.edu>
2020-04-27 23:44:16 -0400

    9

--- crossword.py
+++ crossword.py
@@ -14,4 +14,52 @@ p10_ans = ['broccoli','cucumber','onion','cabbage','corn','mushroom','brusselspr
 
 from PIL import Image
 p1 = Image.open("p1.jpg")
-p1.show()
+p2 = Image.open("p2.jpg")
+p3 = Image.open("p3.jpg")
+p4 = Image.open("p4.jpg")
+p5 = Image.open("p5.jpg")
+p6 = Image.open("p6.jpg")
+p7 = Image.open("p7.jpg")
+p8 = Image.open("p8.jpg")
+p9 = Image.open("p9.jpg")
+p10 = Image.open("p10.jpg")
+picture_list = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
+answer_list = [p1_ans, p2_ans, p3_ans, p4_ans, p5_ans, p6_ans, p7_ans, p8_ans, p9_ans, p10_ans]
+
+
+
+import random
+def playcrossword():
+    correct = 0
+
+    yesorno = input("Want to play a crossword game? [y/n]")
+    while yesorno == "y":
+        #randomly generate a picture from picture list
+        picture = random.choice(picture_list)
+
+        #get the position of the picture in the picture list
+        sequence = picture_list.index(picture)
+
+        #get the answer for the randomly generated picture
+        answer = answer_list[sequence]
+
+        showpic = picture.show()
+        for i in range(len(answer)):
+            user_answer = input("What is the answer for space "+ str(i+1) + "?")
+            if user_answer == answer[i]:
+                correct += 1
+                print("Congrats! You got it right.")
+                print("Your current score is ", str(correct), "/", str(i+1))
+                print()
+            else:
+                print("Sorry, you got it wrong...")
+                print("Your current score is ", str(correct), "/", str(i+1))
+                print()
+
+        print("You finished all!")
+
+        yesorno = input("Want to play a crossword game? [y/n]")
+
+    return("Goodbye!")
+
+playcrossword()
